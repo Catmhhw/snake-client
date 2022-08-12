f695c30113115ca84ff759d3b92532706fec2927
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-12 02:49:15 -0400

    setup stdin

--- play.js
+++ play.js
@@ -1,9 +1,27 @@
 //const net = require("net");
 const connect = require("./client");
 
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
 
+  const handleUserInput = function () {
+    setTimeout(() => {
+      //\u0003 sets up ctrl c to exit?
+      process.stdin.on('data', (key) =>{
+        if (key === '\u0003') {
+          process.exit();
+        }
+      })
+    })
+  };
 
-
+module.exports = {setupInput, handleUserInput};
 
 console.log("Connecting ...");
 connect();
