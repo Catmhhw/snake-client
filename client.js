8eb83bbf07cc49b2c9b72f45cd2ad1a8f744a8b9
Catmhhw <catherine.mhh.wu@gmail.com>
2022-08-11 22:42:04 -0400

    add name message

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 // establishes a connection with the game server
 const net = require("net");
+
 const connect = function () {
     const conn = net.createConnection({
       host: "172.20.39.172",
@@ -10,8 +11,13 @@ const connect = function () {
     conn.setEncoding("utf8");
   
     conn.on("data", (data) => {
-      console.log("Server message:", data)
+      console.log("Server message:", data);
     });
+
+    conn.on("connect", () => {
+        console.log("Successfully connected to the game server");
+        conn.write("Name: CAT");
+    })
   
     return conn;
   };
